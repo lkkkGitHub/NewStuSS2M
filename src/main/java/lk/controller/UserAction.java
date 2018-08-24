424234ed49bd51c0b36b4f528f4ec12b35914ee2
lkkkGitHub <963087276@qq.com>
2018-08-24 11:06:55 +0800

    界面添加；登陆注册进行判断；

--- src/main/java/lk/controller/UserAction.java
+++ src/main/java/lk/controller/UserAction.java
@@ -1,19 +1,27 @@
 package lk.controller;
 
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
 import lk.pojo.User;
 import lk.service.UserService;
+import org.apache.struts2.interceptor.RequestAware;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.servlet.http.Cookie;
+import java.util.Map;
 
 /**
  * @author lk
  * 2018/8/20 14:22
  * @description:
  */
-public class UserAction {
+public class UserAction extends ActionSupport implements RequestAware {
 
     private User user;
+
+    private String passwordTwo;
+
+    private Map<String, Object> request;
 
     public User getUser() {
         return user;
@@ -21,6 +29,19 @@ public class UserAction {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public String getPasswordTwo() {
+        return passwordTwo;
+    }
+
+    public void setPasswordTwo(String passwordTwo) {
+        this.passwordTwo = passwordTwo;
+    }
+
+    @Override
+    public void setRequest(Map<String, Object> map) {
+        this.request = map;
     }
 
     @Autowired
@@ -42,13 +63,11 @@ public class UserAction {
         StringBuffer stringBuffer = new StringBuffer();
         User user1 = userService.loginByMain(user.getMail(), user.getPassword(), stringBuffer);
         if (user1 == null) {
-            return "login";
+            request.put("message", stringBuffer);
+            return Action.INPUT;
         } else {
-            Cookie mailCookie = new Cookie("mail", user.getMail());
-            Cookie passwordCookie = new Cookie("password", user.getPassword());
-            mailCookie.setMaxAge(30);
-            passwordCookie.setMaxAge(30);
-            return "index";
+            ActionContext.getContext().getSession().put("loginUser", user1);
+            return Action.SUCCESS;
         }
     }
 
@@ -66,6 +85,51 @@ public class UserAction {
      */
     public String register() {
         userService.register(user);
-        return "register";
+        return "index";
+    }
+
+    /**
+     * 对所有方法都进行判断
+     */
+    public void validate(){
+
+    }
+
+    /**
+     * 仅对登陆有效
+     */
+    public void validateLogin() {
+        if (user.getMail().isEmpty()) {
+            this.addFieldError("mail", "不能为空");
+        }
+
+        if (user.getPassword().isEmpty()) {
+            this.addFieldError("password", "不能为空");
+        }
+    }
+
+    /**
+     * 仅对注册有效
+     */
+    public void validateRegister(){
+        if (user.getFirstName().isEmpty()) {
+            this.addFieldError("firstName", "不能为空");
+        }
+
+        if (user.getLastName().isEmpty()) {
+            this.addFieldError("lastName", "不能为空");
+        }
+
+        if (user.getMail().isEmpty()) {
+            this.addFieldError("mail", "不能为空");
+        }
+
+        if (user.getPassword().isEmpty()) {
+            this.addFieldError("password", "不能为空");
+        }
+
+        if (!user.getPassword().equals(passwordTwo)) {
+            this.addFieldError("passwordTwo", "与password字段必须一致");
+        }
     }
 }
